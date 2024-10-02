b464f8324d8c4da782a61bbafb29155e1349d688
Zachary A Churchill <zachurchill@aep.com>
2024-10-01 21:56:39 -0400

    figured out how to create methods using black magic and added nice dynamic repr

--- metaprogramming/secrets.py
+++ metaprogramming/secrets.py
@@ -1,4 +1,6 @@
+import inspect
 from abc import ABCMeta, abstractmethod
+from textwrap import dedent
 from typing import List
 
 
@@ -16,6 +18,18 @@ class Secret(metaclass=ABCMeta):
     @abstractmethod
     def show_pwd(self) -> str:
         """Provide raw password for secret"""
+
+    def __repr__(self) -> str:
+        class_name = type(self).__name__
+        properties = {
+            attr: getattr(self, attr)
+            for attr in dir(type(self))
+            if isinstance(getattr(type(self), attr), property)
+        }
+        properties_str = ", ".join(
+            [f'{prop}={val}' for prop, val in properties.items()]
+        )
+        return f"{class_name}({properties_str})"
 
 
 class GenericSecret(Secret):
@@ -49,11 +63,47 @@ class DbSecret(GenericSecret):
 
 class SecretStore:
 
+    @staticmethod
+    def secret_types() -> List[Secret]:
+        return [GenericSecret, DbSecret]
+
     def __init__(self):
         self._secrets: List[Secret] = []
+        self._generate_secret_adders()
+
+    def _generate_secret_adders(self) -> None:
+        """Metaprogramming to create `add_<secret>` functions for `secret_types`."""
+        for secret_type in SecretStore.secret_types():
+            method_name = f"add_{secret_type.__name__.lower()}"
+            method_signature = inspect.signature(secret_type)
+            parameters = ", ".join(method_signature.parameters.keys())
+            method_str = dedent(f"""
+            def {method_name}(self, {parameters}):
+                self._secrets.append({secret_type.__name__}({parameters}))
+            """)
+            local_scope = {}
+            exec(method_str, globals(), local_scope)
+            setattr(self, method_name, local_scope[method_name].__get__(self))
 
     @property
     def secrets(self) -> List[Secret]:
         return self._secrets
 
-    # TODO see if I can use metaprogramming to create `add` methods for each secret type
+
+if __name__ == "__main__":
+    print("Creating a GenericSecret...")
+    generic_secret = GenericSecret("tom", "pwd123!")
+    print(generic_secret)
+
+    print("\nCreating a DbSecret...")
+    dev_db_secret = DbSecret("tom", "dbPwd123!", "localhost:5000")
+    print(dev_db_secret)
+
+    print("\n\nUsing a SecretStore to create secrets:")
+    store = SecretStore()
+    print("First let's see the `add_*` methods created using metaprogramming:")
+    print([attr for attr in dir(store) if attr.startswith("add")])
+    print(f"No secrets starting out: {store.secrets}")
+    store.add_genericsecret("zach", "caspwd123!")
+    store.add_dbsecret("zach", "213ffa", "localhost:9092")
+    print(f"Secrets created using `add_*` methods: {store.secrets}")
